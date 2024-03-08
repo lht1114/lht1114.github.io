e26837449f73de7e49acd732fd27be0661551bfd
lht1114 <liu_wrong@163.com>
2024-03-08 15:17:10 +0800

    优化

---
+++ assets/index-Dd_hZdPp.js
@@ -0,0 +1,1 @@
+import{l as s,j as t}from"./index-Db5lzPrR.js";function e(){const o=s();return console.log(o.pathname),t.jsxs("div",{children:["搜索",o.search]})}export{e as default};
---
+++ assets/index-cNN2l2SZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Db5lzPrR.js";function e(){return r.jsx("div",{children:"用户详情页"})}export{e as default};
