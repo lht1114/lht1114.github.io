806b06802e4395b69f014a3717da3643b68fd1e3
lht1114 <liu_wrong@163.com>
2024-03-08 15:39:13 +0800

    优化

---
+++ assets/index-ByQrO5tw.js
@@ -0,0 +1,1 @@
+import{l as s,j as t}from"./index-C4d1c-c6.js";function e(){const o=s();return console.log(o.pathname),t.jsxs("div",{children:["搜索",o.search]})}export{e as default};
---
+++ assets/singer-DSMkArQm.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index-C4d1c-c6.js";function i(){return r({url:"/top/artists"})}function e(t){return r({url:"artists",params:{id:t}})}export{i as a,e as g};
