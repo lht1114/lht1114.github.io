ce99e9fd661effb6f7c6ddfb557c92e1143e7cbe
lht1114 <liu_wrong@163.com>
2024-03-15 14:15:49 +0800

    优化

--- assets/index-ZkKmrluC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-C4d1c-c6.js";function e(){return r.jsx("div",{children:"用户详情页"})}export{e as default};
--- assets/singer-DSMkArQm.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./index-C4d1c-c6.js";function i(){return r({url:"/top/artists"})}function e(t){return r({url:"artists",params:{id:t}})}export{i as a,e as g};
