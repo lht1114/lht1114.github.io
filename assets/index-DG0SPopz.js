7c06305783980eca8b772768860ca006d81a0bdb
lht1114 <liu_wrong@163.com>
2024-03-16 16:12:15 +0800

    优化

--- assets/index-DG0SPopz.js
+++
@@ -1,1 +0,0 @@
-import{j as r,L as s}from"./index-DTkg-yWw.js";function i(){return r.jsxs("div",{children:["你似乎迷了路",r.jsx(s,{to:"/",children:"回到首页吧"})]})}export{i as default};
--- assets/index-DMCBPgEZ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DTkg-yWw.js";function e(){return t.jsx("div",{children:"设置页面"})}export{e as default};
