e9333df2702982b4624e90c11db034f2e9f02931
lht1114 <liu_wrong@163.com>
2024-03-16 16:12:37 +0800

    优化

---
+++ assets/index-DosnFnsg.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C6S7P4zJ.js";function e(){return t.jsx("div",{children:"设置页面"})}export{e as default};
---
+++ assets/singer-qcXvIbZ5.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-C6S7P4zJ.js";function s(r){return t({url:"/artist/list",params:r})}function e(r){return t({url:"artists",params:{id:r}})}export{s as a,e as g};
